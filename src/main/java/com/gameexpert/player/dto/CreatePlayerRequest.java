d1cea47e23e0e931640e2e5bd800a67206e2728b
Cabbagggggggge <gpwjd7332@naver.com>
2026-09-22 01:03:26 +0900

    [Feat&Fix] 최근 채팅 조회 API 구현/플레이어 등록 코드 일부 수정
    닉네임이 비어있지 않는 조건 코드 작성/채팅 조회 API 구현

--- src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
+++ src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
@@ -1,5 +1,6 @@
 package com.gameexpert.player.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Getter;
 public class CreatePlayerRequest {
 
     // TODO Lv 3: 2~12글자의 영문 대소문자, 숫자와 밑줄을 허용하는 검증을 적용합니다.
+    @NotBlank
     @Size(min = 2, max = 12)
     @Pattern(regexp = "^[a-zA-Z0-9_]+$")
     private final String nickname;

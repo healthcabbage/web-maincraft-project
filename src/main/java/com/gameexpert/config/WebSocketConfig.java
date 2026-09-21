c048900b0320f46a8c76c4511dfe615eb791c9b2
Cabbagggggggge <gpwjd7332@naver.com>
2026-09-22 04:57:26 +0900

    [Feat]HandshakeInterceptor 등록
    websocket 설정에 등록되어있지 부분을 수정

--- src/main/java/com/gameexpert/config/WebSocketConfig.java
+++ src/main/java/com/gameexpert/config/WebSocketConfig.java
@@ -24,7 +24,9 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         // TODO Lv 8: 제공된 인터셉터를 핸들러 등록에 연결합니다.
+
         registry.addHandler(gameWebSocketHandler, "/ws/worlds/{worldId}")
+                .addInterceptors(nicknameInterceptor)
                 .setAllowedOriginPatterns(properties.wsAllowedOrigins().toArray(String[]::new));
     }
 }

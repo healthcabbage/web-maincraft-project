35a7aa7d8945a07b179e7ed5626f7e6583ce3d24
Cabbagggggggge <gpwjd7332@naver.com>
2026-09-22 09:54:53 +0900

    [Feat]플레이어 이동 요청 처리
    클라이언트가 보낸 이동 메시지를 객체 코드 작성 후 게임 엔진에 전달

--- src/main/java/com/gameexpert/ws/handler/MoveWsHandler.java
+++ src/main/java/com/gameexpert/ws/handler/MoveWsHandler.java
@@ -21,5 +21,31 @@ public class MoveWsHandler implements WsMessageHandler {
     public void handle(WsMessageContext context, JsonNode message) {
         String finalSceneActionId = WsFields.optionalFinalSceneActionId(message);
         // TODO Lv 12: 명세의 이동 값을 읽어 현재 사용자의 이동 요청을 엔진에 전달합니다.
+        //위치
+        double x = WsFields.finiteNumber(message, "x");
+        double y = WsFields.finiteNumber(message, "y");
+        double z = WsFields.finiteNumber(message, "z");
+
+        //시선방향
+        float yaw = (float) WsFields.finiteFloat(message, "yaw");
+        float pitch = (float) WsFields.finiteFloat(message, "pitch");
+
+        //이동상태
+        boolean crouching = WsFields.booleanValue(message, "crouching");
+        boolean gliding = WsFields.booleanValue(message, "gliding");
+
+        PlayerAction.Move move = new PlayerAction.Move(
+                context.nickname(),
+                x,
+                y,
+                z,
+                yaw,
+                pitch,
+                crouching,
+                gliding,
+                finalSceneActionId
+        );
+
+        engineManager.enqueue(context.worldId(),  move);
     }
 }
